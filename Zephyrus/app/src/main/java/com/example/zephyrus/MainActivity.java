5680524ac02186de536f71e2e19566504fcc2ffe
Aryapallavi <54701928+Aryapallavi@users.noreply.github.com>
2021-01-11 16:56:44 -0800

    Code changes for Bottom navigation activity to empty activity, Login and Home page set up

--- Zephyrus/app/src/main/java/com/example/zephyrus/MainActivity.java
+++ Zephyrus/app/src/main/java/com/example/zephyrus/MainActivity.java
@@ -1,30 +1,86 @@
 package com.example.zephyrus;
 
-import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.concurrent.Executor;
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
-    }
+        TextView msg_txt = findViewById(R.id.txt_msg);
+        Button login_btn = findViewById(R.id.login_btn);
 
+        BiometricManager biometricManager = BiometricManager.from(this);
+        switch (biometricManager.canAuthenticate()) {
+            case BiometricManager.BIOMETRIC_SUCCESS: //user can use biometric sensor
+//                msg_txt.setText("You can use the fingerprint sensor to login");
+            //msg_txt.setTextColor(Color.parseColor(colorString: "#Fafafa"));
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE: //Biometric sensor not supported in the device
+                msg_txt.setText("The device doesn't have fingerprint sensor");
+                login_btn.setVisibility(View.GONE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE: //Biometric sensor currently not available
+                msg_txt.setText("The biometric sensor is currently unavailable");
+                login_btn.setVisibility(View.GONE);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED://No saved biometrics in the device
+                msg_txt.setText("Device doesn't have a saved fingerprint, please check security settings");
+                login_btn.setVisibility(View.GONE);
+                break;
+        }
+        //Biometric dialog box
+        Executor executor = ContextCompat.getMainExecutor(  this);
+
+        BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override // On error while authentication
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override //On successful authentication
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+               // Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+
+            @Override // On authentication failure
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+
+        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Login")
+                .setDescription("Use your fingerprint to login")
+                .setNegativeButtonText("cancel")
+                .build();
+        // call the dialog on click of login
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            biometricPrompt.authenticate(promptInfo);
+            }
+        });
+
+    }
 }
