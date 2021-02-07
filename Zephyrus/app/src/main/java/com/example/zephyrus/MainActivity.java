a4e6ccd3b6faf60b70cbd82241398871cb76a676
Aryapallavi <54701928+Aryapallavi@users.noreply.github.com>
2021-02-07 10:05:16 -0800

    Server-Client set up

--- Zephyrus/app/src/main/java/com/example/zephyrus/MainActivity.java
+++ Zephyrus/app/src/main/java/com/example/zephyrus/MainActivity.java
@@ -24,61 +24,79 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView msg_txt = findViewById(R.id.txt_msg);
-        Button login_btn = findViewById(R.id.login_btn);
-
-        BiometricManager biometricManager = BiometricManager.from(this);
-        switch (biometricManager.canAuthenticate()) {
-            case BiometricManager.BIOMETRIC_SUCCESS: //user can use biometric sensor
-//                msg_txt.setText("You can use the fingerprint sensor to login");
-            //msg_txt.setTextColor(Color.parseColor(colorString: "#Fafafa"));
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE: //Biometric sensor not supported in the device
-                msg_txt.setText("The device doesn't have fingerprint sensor");
-                login_btn.setVisibility(View.GONE);
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE: //Biometric sensor currently not available
-                msg_txt.setText("The biometric sensor is currently unavailable");
-                login_btn.setVisibility(View.GONE);
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED://No saved biometrics in the device
-                msg_txt.setText("Device doesn't have a saved fingerprint, please check security settings");
-                login_btn.setVisibility(View.GONE);
-                break;
-        }
-        //Biometric dialog box
-        Executor executor = ContextCompat.getMainExecutor(  this);
-
-        BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
-            @Override // On error while authentication
-            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
-                super.onAuthenticationError(errorCode, errString);
-            }
-
-            @Override //On successful authentication
-            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
-               // Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
-                startActivity(intent);
-            }
-
-            @Override // On authentication failure
-            public void onAuthenticationFailed() {
-                super.onAuthenticationFailed();
+        Button server_btn = findViewById(R.id.svr_btn);
+        Button client_btn = findViewById(R.id.clnt_btn);
+//        TextView msg_txt = findViewById(R.id.txt_msg);
+//        Button login_btn = findViewById(R.id.login_btn);
+//
+//        BiometricManager biometricManager = BiometricManager.from(this);
+//        switch (biometricManager.canAuthenticate()) {
+//            case BiometricManager.BIOMETRIC_SUCCESS: //user can use biometric sensor
+////                msg_txt.setText("You can use the fingerprint sensor to login");
+//            //msg_txt.setTextColor(Color.parseColor(colorString: "#Fafafa"));
+//                break;
+//            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE: //Biometric sensor not supported in the device
+//                msg_txt.setText("The device doesn't have fingerprint sensor");
+//                login_btn.setVisibility(View.GONE);
+//                break;
+//            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE: //Biometric sensor currently not available
+//                msg_txt.setText("The biometric sensor is currently unavailable");
+//                login_btn.setVisibility(View.GONE);
+//                break;
+//            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED://No saved biometrics in the device
+//                msg_txt.setText("Device doesn't have a saved fingerprint, please check security settings");
+//                login_btn.setVisibility(View.GONE);
+//                break;
+//        }
+//        //Biometric dialog box
+//        Executor executor = ContextCompat.getMainExecutor(  this);
+//
+//        BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+//            @Override // On error while authentication
+//            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+//                super.onAuthenticationError(errorCode, errString);
+//            }
+//
+//            @Override //On successful authentication
+//            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+//                super.onAuthenticationSucceeded(result);
+//               // Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+//                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+//                startActivity(intent);
+//            }
+//
+//            @Override // On authentication failure
+//            public void onAuthenticationFailed() {
+//                super.onAuthenticationFailed();
+//            }
+//        });
+//
+//        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+//                .setTitle("Login")
+//                .setDescription("Use your fingerprint to login")
+//                .setNegativeButtonText("cancel")
+//                .build();
+//        // call the dialog on click of login
+//        login_btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//            biometricPrompt.authenticate(promptInfo);
+//            }
+//        });
+        server_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+//                Intent intent=new Intent(MainActivity.this,ServerActivity.class);
+//                startActivity(intent);
             }
         });
-
-        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle("Login")
-                .setDescription("Use your fingerprint to login")
-                .setNegativeButtonText("cancel")
-                .build();
-        // call the dialog on click of login
-        login_btn.setOnClickListener(new View.OnClickListener() {
+        client_btn.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-            biometricPrompt.authenticate(promptInfo);
+            public void onClick(View v)
+            {
+//                Intent intent=new Intent(MainActivity.this,ClientActivity.class);
+//             startActivity(intent);
             }
         });
 
