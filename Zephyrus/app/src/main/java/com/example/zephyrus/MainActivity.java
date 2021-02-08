39b55cefb6790368e27384d8dbe1b4e34750c571
Prathyusha Lingamallu <prathyusha.lingamallu@sjsu.edu>
2021-02-07 20:56:04 -0800

    BLE client-server functionality added

--- Zephyrus/app/src/main/java/com/example/zephyrus/MainActivity.java
+++ Zephyrus/app/src/main/java/com/example/zephyrus/MainActivity.java
@@ -17,70 +17,96 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.concurrent.Executor;
+import android.content.Intent;
+import androidx.databinding.DataBindingUtil;
 
+import android.os.Bundle;
+
+import com.example.zephyrus.client.ClientActivity;
+import com.example.zephyrus.databinding.ActivityMainBinding;
+import com.example.zephyrus.server.ServerActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView msg_txt = findViewById(R.id.txt_msg);
-        Button login_btn = findViewById(R.id.login_btn);
+//        Button server_btn = findViewById(R.id.svr_btn);
+//        Button client_btn = findViewById(R.id.clnt_btn);
 
-        BiometricManager biometricManager = BiometricManager.from(this);
-        switch (biometricManager.canAuthenticate()) {
-            case BiometricManager.BIOMETRIC_SUCCESS: //user can use biometric sensor
-//                msg_txt.setText("You can use the fingerprint sensor to login");
-            //msg_txt.setTextColor(Color.parseColor(colorString: "#Fafafa"));
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE: //Biometric sensor not supported in the device
-                msg_txt.setText("The device doesn't have fingerprint sensor");
-                login_btn.setVisibility(View.GONE);
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE: //Biometric sensor currently not available
-                msg_txt.setText("The biometric sensor is currently unavailable");
-                login_btn.setVisibility(View.GONE);
-                break;
-            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED://No saved biometrics in the device
-                msg_txt.setText("Device doesn't have a saved fingerprint, please check security settings");
-                login_btn.setVisibility(View.GONE);
-                break;
-        }
-        //Biometric dialog box
-        Executor executor = ContextCompat.getMainExecutor(  this);
 
-        BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
-            @Override // On error while authentication
-            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
-                super.onAuthenticationError(errorCode, errString);
-            }
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-            @Override //On successful authentication
-            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
-                super.onAuthenticationSucceeded(result);
-               // Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
-                startActivity(intent);
-            }
-
-            @Override // On authentication failure
-            public void onAuthenticationFailed() {
-                super.onAuthenticationFailed();
-            }
-        });
-
-        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
-                .setTitle("Login")
-                .setDescription("Use your fingerprint to login")
-                .setNegativeButtonText("cancel")
-                .build();
-        // call the dialog on click of login
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            biometricPrompt.authenticate(promptInfo);
-            }
-        });
-
+        binding.svrBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this,
+                ServerActivity.class)));
+        binding.clntBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this,
+                ClientActivity.class)));
     }
 }
+
+
+//public class MainActivity extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        TextView msg_txt = findViewById(R.id.txt_msg);
+//        Button login_btn = findViewById(R.id.login_btn);
+//
+//        BiometricManager biometricManager = BiometricManager.from(this);
+//        switch (biometricManager.canAuthenticate()) {
+//            case BiometricManager.BIOMETRIC_SUCCESS: //user can use biometric sensor
+////                msg_txt.setText("You can use the fingerprint sensor to login");
+//            //msg_txt.setTextColor(Color.parseColor(colorString: "#Fafafa"));
+//                break;
+//            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE: //Biometric sensor not supported in the device
+//                msg_txt.setText("The device doesn't have fingerprint sensor");
+//                login_btn.setVisibility(View.GONE);
+//                break;
+//            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE: //Biometric sensor currently not available
+//                msg_txt.setText("The biometric sensor is currently unavailable");
+//                login_btn.setVisibility(View.GONE);
+//                break;
+//            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED://No saved biometrics in the device
+//                msg_txt.setText("Device doesn't have a saved fingerprint, please check security settings");
+//                login_btn.setVisibility(View.GONE);
+//                break;
+//        }
+//        //Biometric dialog box
+//        Executor executor = ContextCompat.getMainExecutor(  this);
+//
+//        BiometricPrompt biometricPrompt = new BiometricPrompt(MainActivity.this, executor, new BiometricPrompt.AuthenticationCallback() {
+//            @Override // On error while authentication
+//            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+//                super.onAuthenticationError(errorCode, errString);
+//            }
+//
+//            @Override //On successful authentication
+//            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+//                super.onAuthenticationSucceeded(result);
+//               // Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+//                Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+//                startActivity(intent);
+//            }
+//
+//            @Override // On authentication failure
+//            public void onAuthenticationFailed() {
+//                super.onAuthenticationFailed();
+//            }
+//        });
+//
+//        BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+//                .setTitle("Login")
+//                .setDescription("Use your fingerprint to login")
+//                .setNegativeButtonText("cancel")
+//                .build();
+//        // call the dialog on click of login
+//        login_btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//            biometricPrompt.authenticate(promptInfo);
+//            }
+//        });
+//
+//    }
+//}
---
+++ Zephyrus/app/src/main/java/com/example/zephyrus/client/GattServerViewModel.java
@@ -0,0 +1,21 @@
+package com.example.zephyrus.client;
+
+import android.bluetooth.BluetoothDevice;
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class GattServerViewModel extends BaseObservable{
+    private BluetoothDevice mBluetoothDevice;
+
+    public GattServerViewModel(BluetoothDevice bluetoothDevice) {
+        mBluetoothDevice = bluetoothDevice;
+    }
+
+    @Bindable
+    public String getServerName() {
+        if (mBluetoothDevice == null) {
+            return "";
+        }
+        return mBluetoothDevice.getAddress();
+    }
+}
